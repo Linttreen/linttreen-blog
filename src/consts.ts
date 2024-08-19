8b01dd85c752f910b0f5292ed571fcd94da83482
Linttreen <150390683+Linttreen@users.noreply.github.com>
2024-08-18 22:49:05 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'linttreen';
+export const SITE_DESCRIPTION = 'Sejam bem-vindos ao meu blog Linttreen, o seu destino para explorar o mundo da programação, design e tecnologia';
